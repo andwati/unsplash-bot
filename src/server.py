b3dec8fbf4d5f4d676ce9dcd356541bbb9af8d62
Ian Andwati <andwatiian@gmail.com>
2022-05-08 00:19:14 +0300

    completed bot logic

---
+++ src/server.py
@@ -0,0 +1,24 @@
+import flask
+import telebot
+
+from bot import bot
+
+app = flask.Flask(__name__)
+
+WEBHOOK_URL_PATH = "/{}".format(bot.token)
+
+
+@app.route(WEBHOOK_URL_PATH, methods=["POST"])
+def webhook():
+    if flask.request.headers.get("content-type") == "application/json":
+        json_string = flask.request.get_data().decode("utf-8")
+        update = telebot.types.Update.de_json(json_string)
+
+        bot.process_new_updates([update])
+        return ""
+    else:
+        flask.abort(403)
+
+
+if __name__ == "__main__":
+    app.run()
