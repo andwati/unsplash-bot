d2e068d527500b637601913a1df607840cfddbaa
Ian Andwati <andwatiian@gmail.com>
2023-09-27 03:20:04 +0300

    implemented random pic functionality

--- unsplashbot/bot.py
+++ unsplashbot/bot.py
@@ -1,5 +1,6 @@
+import requests
 import telebot
-from config import TELEGRAM_TOKEN
+from config import ACCESS_TOKEN, BASE_URL, TELEGRAM_TOKEN
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
@@ -9,9 +10,18 @@ def send_welcome(message):
     bot.reply_to(message, "Hello, I'm a bot!")
 
 
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=["random"])
+def get_random_photo(message):
+    url = "https://api.unsplash.com/photos/random"
+    response = requests.get(
+        url, timeout=10, headers={"Authorization": f"Client-ID {ACCESS_TOKEN}"}
+    )
+    data = response.json()
+    image_url = data["urls"]["regular"]
+    description = data["alt_description"]
+    image_content = requests.get(image_url)
+
+    bot.send_photo(message.chat.id, image_content.content, caption=description)
 
 
 bot.infinity_polling()
--- unsplashbot/config.py
+++ unsplashbot/config.py
@@ -5,3 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+ACCESS_TOKEN = os.getenv("ACCESS_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+BASE_URL = "https://api.unsplash.com"
